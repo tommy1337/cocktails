deb16e1f2e9c0025c6856a9efff2f50304e78400
Tommy <tommy@trinkify.de>
2015-10-06 16:22:32 +0200

    dafuq is this?

--- app/cache/_var_www_html_cocktails_app_views_index.volt.php
+++ app/cache/_var_www_html_cocktails_app_views_index.volt.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>Phalcon PHP Framework</title>
+        <title>Cocktails</title>
+        <link rel="text/css" href="public/Css/style.css">
     </head>
     <body>
         <?php echo $this->getContent(); ?>
